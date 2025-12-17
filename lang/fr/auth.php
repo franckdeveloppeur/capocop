b99c4b09fd877e719c7051b6c696ec7e3d1f4b09
francois-abcd <f.ngamo@allbconsulting.com>
2025-12-17 18:09:23 +0100

    update auth.php

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -88,7 +88,7 @@ return [
 
     // Messages Laravel par défaut
     'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
-    'password' => 'Le mot de passe fourni est incorrect.',
+    'password' => 'Le mot de passe .',
     'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
 
 ];
